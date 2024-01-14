ad17927dd7020a4d455ad770f2e5fc775c3a9b97
EowynPi <syadzwinadhinia@gmail.com>
2024-01-14 23:07:39 +0700

    HELLOWORLD

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Haiiiii</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  tinyLogo: {
+    width: 209,
+    height: 80,
+    marginBottom: '20px',
+    marginLeft: '35px',
   },
 });
